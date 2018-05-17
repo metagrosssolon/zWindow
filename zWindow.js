8945223d5d0512f0a530752d40f46f0a03c9bea1
Vincent L. Solon <32336555+metagrosssolon@users.noreply.github.com>
2018-05-17 23:38:46 +0800

    - Added getComputedStyle() function for getting live css styles.

--- zWindow.js
+++ zWindow.js
@@ -1,3 +1,6 @@
+zsi.__zWindow;
+
+
 $(document).ready(function() {
     $(".zPanel").css({
         "width" : $(window).width()
@@ -11,11 +14,11 @@ $(document).ready(function() {
         ,"position" : "absolute"
     });
     
-    $("body").zWindow({
+    $(".zPanel").zWindow({
         "id" : "window2"
         ,"width" : 200
         ,"height" : 200
-        ,"position" : "fixed"
+        ,"position" : "absolute"
     });
 });
 
@@ -27,17 +30,19 @@ $.fn.zWindow = function(param) {
         // - height
         // - position
         
-        var _$self = this
-            ,_self = this[0]
-            ,_selfStyle = _self.style
+        var _$self              = this
+            ,_self              = this[0]
+            ,_selfStyle         = _self.style
+            ,_selfCompStyle     = window.getComputedStyle(_self)
             ,_$zWindow
             ,_zWindow
             ,_zWindowStyle
+            ,_zWindowCompStyle
             
             ,_drag
             ,_resize
-            ,_subtrahend = {}
-            ,_onMouseDownObj = {}
+            ,_subtrahend        = {}
+            ,_onMouseDownObj    = {}
         ;
         
         _$self.append(
@@ -62,6 +67,10 @@ $.fn.zWindow = function(param) {
         
         _zWindow = _$zWindow[0];
         _zWindowStyle = _zWindow.style;
+        _zWindowCompStyle = window.getComputedStyle(_zWindow);
+        
+        
+        
         
         _$zWindow.mousedown(function(e) {
             var __target = e.target;
@@ -69,9 +78,9 @@ $.fn.zWindow = function(param) {
             if (__target.id === param.id) {
                 _drag = true;
                 
-                if (_zWindowStyle.position === "absolute") {
+                if (_zWindowCompStyle.position === "absolute") {
                     // zWindow is absolute
-                    if (_selfStyle.position === "relative") {
+                    if (_selfCompStyle.position === "relative") {
                         _subtrahend.y = _self.offsetTop + e.offsetY;
                         _subtrahend.x = _self.offsetLeft + e.offsetX;
                     } else {
@@ -101,9 +110,10 @@ $.fn.zWindow = function(param) {
             e.preventDefault();
         });
         
+        
         _$self.mousemove(function(e) {
             if (_drag) {
-                if (_zWindowStyle.position === "absolute") {
+                if (_zWindowCompStyle.position === "absolute") {
                     // zWindow is absolute
                     _zWindowStyle.top = e.pageY - _subtrahend.y + "px";
                     _zWindowStyle.left = e.pageX - _subtrahend.x + "px";
@@ -117,31 +127,31 @@ $.fn.zWindow = function(param) {
             }
             
             if (_resize) {
-                var __resizer = _onMouseDownObj.resizer
-                    ,__pageX = e.pageX
-                    ,__pageY = e.pageY
-                    ,__lastPageX = _onMouseDownObj.pageX
-                    ,__lastPageY = _onMouseDownObj.pageY
-                    ,__lastWidth = _onMouseDownObj.width
-                    ,__lastHeight = _onMouseDownObj.height
-                    ,__lastOffsetTop = _onMouseDownObj.offsetTop
-                    ,__lastOffsetLeft = _onMouseDownObj.offsetLeft
-                    ,__newWidth = 0
-                    ,__newHeight = 0
+                var __resizer           = _onMouseDownObj.resizer
+                    ,__pageX            = e.pageX
+                    ,__pageY            = e.pageY
+                    ,__lastPageX        = _onMouseDownObj.pageX
+                    ,__lastPageY        = _onMouseDownObj.pageY
+                    ,__lastWidth        = _onMouseDownObj.width
+                    ,__lastHeight       = _onMouseDownObj.height
+                    ,__lastOffsetTop    = _onMouseDownObj.offsetTop
+                    ,__lastOffsetLeft   = _onMouseDownObj.offsetLeft
+                    ,__newWidth         = 0
+                    ,__newHeight        = 0
                     
-                    ,__pageXPos = __lastPageX - __pageX
-                    ,__pageXNeg = __pageX - __lastPageX
-                    ,__pageYPos = __lastPageY - __pageY
-                    ,__pageYNeg = __pageY - __lastPageY
+                    ,__pageXA         = __lastPageX - __pageX
+                    ,__pageXB         = __pageX - __lastPageX
+                    ,__pageYA         = __lastPageY - __pageY
+                    ,__pageYB         = __pageY - __lastPageY
                 ;
                 
                 if (__resizer === "left" || __resizer === "top-left" || __resizer === "bottom-left")
                 {
-                    __newWidth = (__pageX > __lastPageX ? __lastWidth + __pageXPos : __lastWidth - __pageXNeg);
+                    __newWidth = (__pageX > __lastPageX ? __lastWidth + __pageXA : __lastWidth - __pageXB);
                     
                     if (__newWidth > 200) {
                         _zWindowStyle.width = __newWidth + "px";
-                        _zWindowStyle.left = (__lastOffsetLeft - __pageXPos) + "px";
+                        _zWindowStyle.left = (__lastOffsetLeft - __pageXA) + "px";
                     } else {
                         _zWindowStyle.width = "200px";
                         _zWindowStyle.left = ((__lastOffsetLeft + __lastWidth) - 200) + "px";
@@ -150,17 +160,17 @@ $.fn.zWindow = function(param) {
                 
                 if (__resizer === "right" || __resizer === "bottom-right" || __resizer === "top-right")
                 {
-                    __newWidth = (__pageX > __lastPageX ? __lastWidth + __pageXNeg : __lastWidth - __pageXPos);
+                    __newWidth = (__pageX > __lastPageX ? __lastWidth + __pageXB : __lastWidth - __pageXA);
                     _zWindowStyle.width = (__newWidth > 200 ? __newWidth : 200) + "px";
                 }
                 
                 if (__resizer === "top" || __resizer === "top-left" || __resizer === "top-right")
                 {
-                    __newHeight = (__pageY > __lastPageY ? __lastHeight + __pageYPos : __lastHeight - __pageYNeg);
+                    __newHeight = (__pageY > __lastPageY ? __lastHeight + __pageYA : __lastHeight - __pageYB);
                     
                     if (__newHeight > 200) {
                         _zWindowStyle.height = __newHeight + "px";
-                        _zWindowStyle.top = (__lastOffsetTop - __pageYPos) + "px";
+                        _zWindowStyle.top = (__lastOffsetTop - __pageYA) + "px";
                     } else {
                         _zWindowStyle.height = "200px";
                         _zWindowStyle.top = ((__lastOffsetTop + __lastHeight) - 200) + "px";
@@ -169,24 +179,22 @@ $.fn.zWindow = function(param) {
                 
                 if (__resizer === "bottom" || __resizer === "bottom-left" || __resizer === "bottom-right")
                 {
-                    __newHeight = (__pageY > __lastPageY ? __lastHeight + __pageYNeg : __lastHeight - __pageYPos);
-                    _zWindowStyle.height = (__newHeight > 200? __newHeight : 200) + "px";
+                    __newHeight = (__pageY > __lastPageY ? __lastHeight + __pageYB : __lastHeight - __pageYA);
+                    _zWindowStyle.height = (__newHeight > 200 ? __newHeight : 200) + "px";
                 }
                 
                 return false;
             }
-        }).mouseup(function(e) {
-            if (_drag) {
-                _zWindow.className = _zWindow.className.replace(/ active/i,"");
-                
-                _drag = false;
-                return false;
-            }
-            
+        }).mouseup(function() {
+            _zWindow.className = _zWindow.className.replace(/ active/i,"");
+            _drag = false;
+            _resize = false;
+        }).mouseleave(function() {
             if (_resize) {
-                
-                _resize = false;   
-                return false;
+                if (_zWindowCompStyle.position === "absolute") {
+                    _resize = false;
+                    return false;
+                }
             }
         });
     }
